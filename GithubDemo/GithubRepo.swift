80d34cb86ad79124a6ab298e2c870a8e094a5075
Jungyoon Yu <yu612@purdue.edu>
2017-02-16 18:45:08 -0500

    Added description

--- GithubDemo/GithubRepo.swift
+++ GithubDemo/GithubRepo.swift
@@ -21,6 +21,7 @@ class GithubRepo: CustomStringConvertible {
     var ownerAvatarURL: String?
     var stars: Int?
     var forks: Int?
+    var about: String?
     
     // Initializes a GitHubRepo from a JSON dictionary
     init(jsonResult: NSDictionary) {
@@ -43,6 +44,10 @@ class GithubRepo: CustomStringConvertible {
             if let ownerAvatarURL = owner["avatar_url"] as? String {
                 self.ownerAvatarURL = ownerAvatarURL
             }
+        }
+        
+        if let about = jsonResult["description"] as? String? {
+                self.about = about
         }
     }
     
@@ -98,6 +103,7 @@ class GithubRepo: CustomStringConvertible {
             "\n\t[Stars: \(self.stars!)]" +
             "\n\t[Forks: \(self.forks!)]" +
             "\n\t[Owner: \(self.ownerHandle!)]" +
-            "\n\t[Avatar: \(self.ownerAvatarURL!)]"
+            "\n\t[Avatar: \(self.ownerAvatarURL!)]" +
+            "\n\t[Description: \(self.about!)]"
     }
 }
