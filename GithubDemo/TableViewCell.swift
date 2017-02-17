fa7c68441def33463ba23c74d1cd7c8fc2ed50c7
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2017-02-16 20:07:19 -0500

    AutoLayout havn't finished

--- GithubDemo/TableViewCell.swift
+++ GithubDemo/TableViewCell.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AFNetworking
 
 class TableViewCell: UITableViewCell {
 
     @IBOutlet var repoName: UILabel!
+    @IBOutlet var owner: UILabel!
+  
+    @IBOutlet var forkCount: UILabel!
+    @IBOutlet var starCount: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var avater: UIImageView!
+    @IBOutlet var star: UIImageView!
+    @IBOutlet var fork: UIImageView!
+    
     var repo: GithubRepo!
     {
         didSet
         {
             repoName.text = repo.name
+            owner.text = "by " + repo.ownerHandle!
+            star.image = #imageLiteral(resourceName: "star")
+            
+            avater.setImageWith(URL(string: "\(repo.ownerAvatarURL!)")!)
+            descriptionLabel.text = repo.about
+            forkCount.text = "\(repo.forks)"
+            starCount.text = "\(repo.stars)"
+            
         }
     }
     override func awakeFromNib() {
